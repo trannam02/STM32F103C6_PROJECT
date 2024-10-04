53e9a8782331994923b8bb5a69be361f505aa6e3
Tran Xuan Nam <tranxuannam02@gmail.com>
2024-10-04 15:01:36 +0700

    update counter time

--- Core/Src/update7SEG.c
+++ Core/Src/update7SEG.c
@@ -3,24 +3,43 @@
 const int MAX_LED = 4;
 int index_led = 0;
 int dot_state = ON;
-int led_buffer[4] = { 1, 2, 3, 4 };
+int hour = 15;
+int minute = 59;
+int second = 58;
+int led_buffer[4] = { 1, 5, 5, 9 };
 
 void update7SEG_run() {
 	if (getFlag(0) == 1) {
 		update7SEG(++index_led % 4);
-		setTimer(0, 500);
-	}
-	;
+		setTimer(0, 250);
+	};
 	if (getFlag(1) == 1) {
-		if(dot_state == 1) {
+		if (dot_state == 1) {
 			disableDOT();
 			dot_state = OFF;
-		}
-		else {
+		} else {
 			enableDOT();
 			dot_state = ON;
 		};
-		setTimer(1,1000);
+		// calc time
+		second++;
+		if (second >= 60) {
+			second = 0;
+			minute++;
+		};
+		if (minute >= 60) {
+			minute = 0;
+			hour++;
+		};
+		if (hour >= 24) {
+			hour = 0;
+		};
+
+		led_buffer[0] = hour / 10;
+		led_buffer[1] = hour % 10;
+		led_buffer[2] = minute / 10;
+		led_buffer[3] = minute % 10;
+		setTimer(1, 1000);
 	};
 }
 ;
@@ -36,7 +55,7 @@ void update7SEG_init() {
 	enableDOT();
 
 	// init timer
-	setTimer(0, 500);
+	setTimer(0, 250);
 	setTimer(1, 1000);
 }
 ;
